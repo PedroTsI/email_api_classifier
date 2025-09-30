a5d430d7b9607264990ba1b769caaeca561278a2
PedroTsI <ppcantalice121@gmail.com>
2025-09-30 09:16:38 -0300

    Deploy config

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import uvicorn
-from api import app # Importa a instância 'app' do módulo api
+from app import app # Importa a instância 'app' do módulo api
 
 # Bloco de Execução
 if __name__ == "__main__":
     # ESSENCIAL: 'api:app' é a referência ao objeto FastAPI dentro do módulo api.py
     # Usamos o reload=True para desenvolvimento.
-    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
